41f75fe7b29b32ec80f81581bab76249c7644752
ValeriuOvidiu <126098277+ValeriuOvidiu@users.noreply.github.com>
2023-03-29 12:27:59 +0300

    Update spanzuratoarea.js

--- spanzuratoarea.js
+++ spanzuratoarea.js
@@ -1,67 +1,45 @@
-
 let word;
 let numberOfLetters;
 let sendToHtml = new Array(numberOfLetters)
-
-function salveaza() {
-   const theText = document.getElementById('input1')
-   const valueOfText = new String(theText.value)
-   let lenghtOfText = valueOfText.length
-   numberOfLetters = lenghtOfText
-   word = valueOfText
-   let i = 0
-   while (lenghtOfText > 0) {
+function save() {
+   const theText = document.getElementById('wordInput').value
+   numberOfLetters = theText.length
+   word = theText
+   for (let i = 0; i < numberOfLetters; ++i) {
       sendToHtml[i] = ' _'
-      --lenghtOfText;
-      ++i
+      document.getElementById('secondOutput').innerHTML += sendToHtml[i]
    }
-   for (let y = 0; y < numberOfLetters; ++y) {
-      document.getElementById('secondOutput').innerHTML += sendToHtml[y]
-   }
-   console.log(valueOfText)
-   console.log(lenghtOfText)
-   theText.value = ''
+   document.getElementById('wordInput').value = ''
    document.getElementById("Button").disabled = true;
    document.getElementById("secondButton").disabled = false;
-
-
 }
 
 let wrong = 1
 let winner = 0;
-
-function compara() {
+function letterProcessing() {
    document.getElementById('secondOutput').innerHTML = ''
-   const letterImput = document.getElementById('input2')
-   const letterAsString = letterImput.value
-   let numberOfLettersCopy = numberOfLetters
-   let j = 0;
+   const letterAsString = document.getElementById('letterInput').value
    let wrongCounter = 1;
-   if (letterImput.value != '') {
-      while (numberOfLettersCopy > 0) {
+   if (letterAsString != '') {
+      for (let j = 0; j < numberOfLetters; ++j) {
          if (word.charAt(j) != letterAsString.charAt(0) && sendToHtml[j] == undefined) {
             sendToHtml[j] = ' _'
-
          } else if ((sendToHtml[j] == undefined || sendToHtml[j] == ' _') &&
             word.charAt(j) == letterAsString.charAt(0)) {
             sendToHtml[j] = ' ' + word.charAt(j)
             ++winner
             ++wrongCounter
          }
-         ++j
-         --numberOfLettersCopy
       }
-      
       if (wrongCounter == 1) {
          ++wrong
-         console.log(wrong)
          if (wrong < 12) {
-            document.getElementById('imagine').src = "spanzuratoarea"+wrong+".jpg"
-         }  else if (wrong == 12) {
+            document.getElementById('imagine').src = "spanzuratoarea" + wrong + ".jpg"
+         } else if (wrong == 12) {
             document.getElementById('imagine').src = "spanzuratoarea12.jpg"
             document.getElementById('secondOutput').innerHTML = word
             numberOfLetters = -1
-            document.getElementById('output').innerHTML = "Ai pierdut \n Cuvantul era-"
+            document.getElementById('output').innerHTML = "Ai pierdut, cuvantul era-"
             document.getElementById("secondButton").disabled = true;
          }
       }
@@ -73,8 +51,9 @@ function compara() {
    for (let y = 0; y < numberOfLetters; ++y) {
       document.getElementById('secondOutput').innerHTML += sendToHtml[y]
    }
-   letterImput.value = ''
+   document.getElementById('letterInput').value = ''
 }
+
 function restart() {
    document.getElementById("Button").disabled = false;
    document.getElementById("secondButton").disabled = false;
